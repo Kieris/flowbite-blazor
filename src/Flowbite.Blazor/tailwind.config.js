373ba57253986be9e008fdbb7cc062b6f7c5c2e6
Joshua Garren <garrenjl@gmail.com>
2023-09-27 13:03:34 -0500

    Accordion Functionality Working

--- src/Flowbite.Blazor/tailwind.config.js
+++ src/Flowbite.Blazor/tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{html,js,razor,ts,md}',
+    './**/*.{html,js,razor,cshtml,ts,md}',
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
+   
   ],
   darkMode: 'class',
 }
