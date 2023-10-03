9c495f230ced11baaf58cdcbb17e527bc5e79060
Josh Garren <garrenjl@gmail.com>
2023-10-02 19:33:57 -0500

    feat:added sidebar components

---
+++ src/Flowbite.Blazor/Models/SiteType.cs
@@ -0,0 +1,10 @@
+namespace Flowbite.Blazor.Models;
+
+public class SiteType
+{
+    public string Name { get; set; } = string.Empty;
+    public string Url { get; set; } = string.Empty;
+    public string ImgUrl { get; set; } = string.Empty;
+    
+    
+}
