3348f89a83f6be3d010cd1f558143569ae8008e6
Joshua Garren <garrenjl@gmail.com>
2023-10-09 18:57:37 -0500

    feat: added skeletons

---
+++ src/Flowbite.Blazor/Components/Skeleton/SkSize.cs
@@ -0,0 +1,13 @@
+﻿namespace Flowbite.Blazor.Components.Skeleton;
+
+/// <summary>
+/// Defines the size of the skeleton
+/// </summary>
+public enum SkSize
+{
+    Sm,
+    Md,
+    Lg,
+    Xl,
+    Xxl
+}
