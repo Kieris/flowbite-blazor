00897f623a7277807476f41556dc78bc9ae3464e
Josh Garren <garrenjl@gmail.com>
2023-10-10 19:33:52 -0500

    feat: added drawer

---
+++ src/Flowbite.Blazor/SharedEnums/Placement.cs
@@ -0,0 +1,12 @@
+namespace Flowbite.Blazor.SharedEnums;
+
+/// <summary>
+/// Defines the placement of a component
+/// </summary>
+public enum Placement
+{
+    Top,
+    Bottom,
+    Left,
+    Right,
+}
