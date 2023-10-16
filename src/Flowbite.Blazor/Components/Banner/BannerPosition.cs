009318ac3b30fc9f65cbac86527ff5d3bdb20a0f
Joshua Garren <garrenjl@gmail.com>
2023-10-15 22:46:37 -0500

    feat: added banner

---
+++ src/Flowbite.Blazor/Components/Banner/BannerPosition.cs
@@ -0,0 +1,13 @@
+﻿namespace Flowbite.Blazor.Components.Banner;
+
+/// <summary>
+/// Defines the position for the banner
+/// </summary>
+public enum BannerPosition
+{
+    Absolute,
+    Fixed,
+    Relative,
+    Sticky,
+    Static
+}
---
+++ src/Flowbite.Blazor/Components/Banner/BannerType.cs
@@ -0,0 +1,13 @@
+﻿namespace Flowbite.Blazor.Components.Banner;
+
+/// <summary>
+/// Defines the type for the banner
+/// </summary>
+public enum BannerType
+{
+    Default, 
+    Bottom,
+    Cta,
+    Signup,
+    Info
+}
